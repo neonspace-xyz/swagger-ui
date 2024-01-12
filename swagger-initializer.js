22ec7a16149896234e2c517e86624c9646cdc9c9
syahrul <muhdsyahrulnizam123@gmail.com>
2024-01-12 15:31:10 +0800

    rename url

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/neonspace-xyz/swagger-ui/main/swagger.yaml",
+    url: "https://raw.githubusercontent.com/neonspace-xyz/swagger-ui/main/swagger.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
